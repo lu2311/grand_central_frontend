a1f869bfa65813379de92b82f0e3ff7597a36969
naomi gonzales <u22240683@utp.edu.pe>
2025-09-18 00:19:07 -0500

    Delete src directory

--- src/components/BackToTop.js
+++
@@ -1,7 +0,0 @@
-export default function BackToTop() {
-  return (
-    <a href="#" className="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top">
-      <i className="bi bi-arrow-up"></i>
-    </a>
-  );
-}
