22974a4e61d248de0e376b91cd1d6aee9bccd528
lu2311 <luisyraita2306@gmail.com>
2025-11-12 19:59:52 -0500

    Importacion de ProtectedAdmin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import Admin from "./pages/Admin";
+import ProtectedRouteAdmin from "./components/ProtectedRouteAdmin";
 
 
 function App() {
