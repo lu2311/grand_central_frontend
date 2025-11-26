24d86201deb2e02848b55fa07a74406ae3a45ced
lu2311 <luisyraita2306@gmail.com>
2025-11-26 14:52:29 -0500

    Correccion

--- src/components/admin/AdminMenu.js
+++ src/components/admin/AdminMenu.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Swal from "sweetalert2";
-import api from "../utils/Api";
+import api from "../../utils/Api";
 
 function Menu() {
   const [menu, setMenu] = useState([]);
